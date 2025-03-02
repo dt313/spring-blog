0b4686ed5f793d9eabc54aa74720fcb51a099685
dt313 <dt313.dev21@gmail.com>
2025-03-02 14:00:24 +0900

    fix:ws

--- src/main/java/com/blog/api/configuration/UserInterceptor.java
+++ src/main/java/com/blog/api/configuration/UserInterceptor.java
@@ -11,16 +11,23 @@ import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.util.LinkedMultiValueMap;
 
+import java.util.Objects;
+
 @Slf4j
 public class UserInterceptor implements ChannelInterceptor {
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
-        if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+
+        if (StompCommand.CONNECT.equals(accessor.getCommand()) || Objects.isNull(accessor.getUser())) {
+            System.out.println("connect ");
             Object raw = message.getHeaders().get(SimpMessageHeaderAccessor.NATIVE_HEADERS);
             String userId = (String) ((LinkedMultiValueMap) raw).get("userId").get(0);
             accessor.setUser(new UserPrincipal(userId));
         }
+
+        System.out.println("USER ID --- " + accessor.getUser());
+
         return ChannelInterceptor.super.preSend(message, channel);
     }
 }
